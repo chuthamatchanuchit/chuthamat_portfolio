89aaac64b2e3e0bab05f1a9f20689895d2b245c9
chuthamatchanuchit <614259005@webmail.npru.ac.th>
2020-10-05 21:25:33 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+    "name": "chuthamat chanuchit",
+    "aboutme":{
+        "FullName":"chuthamat",
+        "Phone":"0983203394",
+        "Email":"chuthamatchanuchit@gmail.com",
+        "Address":"122, Danchang , Danchang , Suphan Buri 72180",
+    
+    },
+    "education":{
+        "Upper":"banharn3 English-chinese class",
+        "lowers":"banharn3"
+    },
+    "workexperience":{
+      "1":"play Sports.",
+      "2":"musician in my spare time."
+    }
+
+};
+module.exports = data;
